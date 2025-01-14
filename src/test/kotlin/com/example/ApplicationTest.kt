ac2e853344c90be6ed6910b603763ed21256702d
Alexander Zheleznov <zheleznov.a.g@gmail.com>
2025-01-14 21:48:14 +0700

    Red

---
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -0,0 +1,21 @@
+package com.example
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+
+class ApplicationTest {
+
+    @Test
+    fun `test root endpoint`() = testApplication {
+        application {
+            module()
+        }
+        val response = client.get("/")
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("Hello World!", response.bodyAsText())
+    }
+}
